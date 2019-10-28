c0ee7b72569b44ac241164554aa58ad585a3ada4
AlexeyVoloshin <voloshinalex1100@gmail.com>
2019-10-28 14:03:41 +0200

    add google map radius

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,31 +1,77 @@
-import {Component, NgZone, OnInit} from '@angular/core';
+import {Component, EventEmitter, NgZone, OnInit} from '@angular/core';
 import {MapsAPILoader} from '@agm/core';
 import {FormControl} from '@angular/forms';
-
+// tslint:disable-next-line:class-name
+interface marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
+}
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-  public latitude: number;
-  public longitude: number;
-  public locationChosen = false;
-  public searchControl: FormControl;
-  public zoom: number;
-  constructor(
-    private mapsAPILoader: MapsAPILoader,
-    private ngZone: NgZone
-  ) { }
+  zoom: number = 8;
 
-  ngOnInit() {
-    this.zoom = 4;
-    this.latitude = 47.017950;
-    this.longitude = 28.812735;
+  // initial center position for the map
+  lat: number = 51.673858;
+  lng: number = 7.815982;
+  radius: number = 5000;
+
+    ngOnInit() {
+    }
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`);
   }
-  onChoseLocation(event) {
-    this.latitude = event.coords.lat;
-    this.longitude = event.coords.lng;
-    this.locationChosen = true;
+
+  mapClicked(event) {
+    this.markers.push({
+      lat: event.coords.lat,
+      lng: event.coords.lng,
+      draggable: true
+    });
   }
+
+  markerDragEnd(m: marker, event) {
+     this.lat = event.coords.lat;
+     this.lng = event.coords.lng;
+    console.log('dragEnd', m, event);
+  }
+
+  markers: marker[] = [
+    {
+      lat: 51.673858,
+      lng: 7.815982,
+      label: 'A',
+      draggable: true
+    },
+    {
+      lat: 51.373858,
+      lng: 7.215982,
+      label: 'B',
+      draggable: false
+    },
+    {
+      lat: 51.723858,
+      lng: 7.895982,
+      label: 'C',
+      draggable: true
+    }
+  ]
+
+  radiusChange(event) {
+      this.radius = event;
+    console.log('RadiusX', event);
+  }
+}
+
+// just an interface for type safety.
+interface marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
 }
